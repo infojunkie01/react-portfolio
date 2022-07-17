dd76347ccf2612a7c36318fa7bef89ebc356cbaa
Jessica Wang <jessica.l.wang01@gmail.com>
2022-07-16 22:11:08 -0400

    starts sets up basic html for contact section

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,7 +3,22 @@ import React from 'react';
 function Contact() {
   return (
     <section>
-      <h2 id="about" className="d-flex justify-content-center ">Contact</h2>
+      <h2 id="about" className="d-flex justify-content-center ">Contact me</h2>
+      <form id="contact-form" className='form'>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input className='mx-1' type="text" name="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input className='mx-1' type="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" className='mx-1' rows="5"  />
+        </div>
+        <button data-testid="button" type="submit">Submit</button>
+      </form>
     </section>
   );
 }
